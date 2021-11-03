0ac3d9b64bba5cd10d00d36704b8d3103cb544e8
Gabriel <alltaircs@gmail.com>
2021-11-03 13:21:14 +0100

    feat: added account model

---
+++ Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required, ] public string AccountName { get; set; }
+
+        [Required] public string Password { get; set; }
+    }
+}
