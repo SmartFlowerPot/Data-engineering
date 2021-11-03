608d8aefd678c3f950c2ee70cf52a33a23c46cf4
Gabriel <alltaircs@gmail.com>
2021-11-03 14:17:55 +0100

    fix: account model

--- Models/Account.cs
+++ Models/Account.cs
@@ -4,10 +4,7 @@ namespace WebAPI.Models
 {
     public class Account
     {
-        [Key]
-        public int Id { get; set; }
-        
-        [Required, ] public string AccountName { get; set; }
+        [Required, Key] public string AccountName { get; set; }
 
         [Required] public string Password { get; set; }
     }
