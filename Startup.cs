7f850b5ca5ed68cee8e8372256d8690fc990488a
asterixcode <lk.saldanha@gmail.com>
2021-11-08 21:50:40 +0100

    build: api deployment into azure app service

--- Startup.cs
+++ Startup.cs
@@ -43,6 +43,11 @@ namespace WebAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
 
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+            
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
