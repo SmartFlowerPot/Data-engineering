cd590b1f9b18c2478d1227bdee4f3d88546710f8
Pupendo <michal.pupak@gmail.com>
2021-12-12 11:48:43 +0100

    feat: window control endpoint & Loriot communication, integration tests

--- Tests/PlantControllerTest.cs
+++ Tests/PlantControllerTest.cs
@@ -13,39 +13,26 @@ namespace Tests
     public class PlantControllerTest : IntegrationTest
     {
         private readonly ITestOutputHelper _testOutputHelper;
+        private readonly MeasurementPersistence _persistence;
 
         private readonly Plant _plant;
         private readonly Account _account;
-
-        private const string ValidUsername = "PLANT TEST USERNAME";
-        private const string PlantNickname = "TEST PLANT";
-        private const string ValidEui = "123465789";
+        
+        private const string ValidEui = "qwerty";
         private const string InvalidEui = "696969696";
 
         public PlantControllerTest(ITestOutputHelper outputHelper)
         {
             _testOutputHelper = outputHelper;
-            _plant = new Plant
-            {
-                DOB = new DateTime(2019, 09, 23),
-                EUI = ValidEui,
-                Nickname = PlantNickname
-            };
-            _account = new Account()
-            {
-                Username = ValidUsername,
-                DateOfBirth = DateTime.Now,
-                Password = "password",
-                Gender = "Male"
-            };
+            _persistence = new MeasurementPersistence();
         }
 
         [Fact]
         public async Task GetPlantInvalidEuiTest()
         {
             //Arrange
-            await DeletePlantAsync();
-            await PersistPlantAsync();
+            await _persistence.DeleteMeasurement();
+            await _persistence.PersistMeasurement();
             
             //Act
             var response = await TestClient.GetAsync($"{Https}/plant?eui={InvalidEui}");
@@ -55,15 +42,15 @@ namespace Tests
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
             
             //Clean up
-            await DeletePlantAsync();
+            await _persistence.DeleteMeasurement();
         }
         
         [Fact]
         public async Task GetPlantValidEuiTest()
         {
             //Arrange
-            await DeletePlantAsync();
-            await PersistPlantAsync();
+            await _persistence.DeleteMeasurement();
+            await _persistence.PersistMeasurement();
             
             //Act
             var response = await TestClient.GetAsync($"{Https}/plant?eui={ValidEui}");
@@ -76,49 +63,40 @@ namespace Tests
             //Assert
             _testOutputHelper.WriteLine("RESPONSE: "+response.Content.ReadAsStringAsync().Result);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            if (plant != null) Assert.Equal(PlantNickname, plant.Nickname);
+            if (plant != null) Assert.Equal("Vladimir Trump", plant.Nickname);
+            
             //Clean up
-            await DeletePlantAsync();
+            await _persistence.DeleteMeasurement();
         }
 
         [Fact]
         public async Task RemovePlantTest()
         {
             //Arrange
-            await DeletePlantAsync();
-            await PersistPlantAsync();
+            await _persistence.DeleteMeasurement();
+            await _persistence.PersistMeasurement();
             
             //Act
             var response = await TestClient.GetAsync($"{Https}/plant?eui={ValidEui}");
             var response1 = await TestClient.DeleteAsync($"{Https}/plant?eui={ValidEui}");
             var response2 = await TestClient.GetAsync($"{Https}/plant?eui={ValidEui}");
             
+            //Test if all measurements for that plant are also deleted 
+            var response3 = await TestClient.GetAsync($"{Https}/temperature?eui={ValidEui}");
+            
             //Assert
             _testOutputHelper.WriteLine("RESPONSE 0: "+response.Content.ReadAsStringAsync().Result);
             _testOutputHelper.WriteLine("RESPONSE 1: "+response1.Content.ReadAsStringAsync().Result);
             _testOutputHelper.WriteLine("RESPONSE 2: "+response2.Content.ReadAsStringAsync().Result);
+            _testOutputHelper.WriteLine("RESPONSE 3: "+response3.Content.ReadAsStringAsync().Result);
             
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.Equal(HttpStatusCode.OK, response1.StatusCode);
             Assert.Equal(HttpStatusCode.NotFound, response2.StatusCode);
+            Assert.Equal(HttpStatusCode.NotFound, response2.StatusCode);
             
             //Clean up
-            await DeletePlantAsync();
-        }
-
-        private async Task PersistPlantAsync()
-        {
-            IAccountRepo accountRepo = new AccountRepo();
-            await accountRepo.PostAccountAsync(_account);
-            
-            IPlantRepo repo = new PlantRepo();
-            await repo.PostPlantAsync(_plant, ValidUsername);
-        }
-        
-        private async Task DeletePlantAsync()
-        {
-            IAccountRepo accountRepo = new AccountRepo();
-            await accountRepo.DeleteAccountAsync(_account.Username);
+            await _persistence.DeleteMeasurement();
         }
     }
 }
