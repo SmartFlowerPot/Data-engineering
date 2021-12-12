b8bce64d9d1ed8be3042db75eafa5b0b2308b88e
asterixcode <lk.saldanha@gmail.com>
2021-12-12 19:14:25 +0100

    fix: clean up

--- WebAPI/Models/Account.cs
+++ WebAPI/Models/Account.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 
 namespace WebAPI.Models
 {
